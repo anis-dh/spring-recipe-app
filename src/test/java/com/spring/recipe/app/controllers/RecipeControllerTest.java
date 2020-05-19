93a1595c077f983f908a813eb254c9387b03db0b
anis <anis.dh94@gmail.com>
2020-05-19 02:47:33 +0100

    fix errors

--- src/test/java/com/spring/recipe/app/controllers/RecipeControllerTest.java
+++ src/test/java/com/spring/recipe/app/controllers/RecipeControllerTest.java
@@ -3,7 +3,6 @@ package com.spring.recipe.app.controllers;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-<<<<<<< HEAD
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
@@ -43,45 +42,5 @@ public class RecipeControllerTest {
 
     mockMvc.perform(get("/recipe/show/1")).andExpect(status().isOk())
         .andExpect(view().name("recipe/show")).andExpect(model().attributeExists("recipe"));
-=======
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import com.spring.recipe.app.domain.Recipe;
-import com.spring.recipe.app.services.RecipeService;
-
-public class RecipeControllerTest {
-
-
-  @Mock
-  RecipeService recipeService;
-
-  RecipeController controller;
-
-  @Before
-  public void setUp() throws Exception {
-    MockitoAnnotations.initMocks(this);
-
-    controller = new RecipeController(recipeService);
-  }
-
-  @Test
-  public void testGetRecipe() throws Exception {
-
-    Recipe recipe = new Recipe();
-    recipe.setId(1L);
-
-    MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
-
-    when(recipeService.findById(anyLong())).thenReturn(recipe);
-
-    mockMvc.perform(get("/recipe/show/1")).andExpect(status().isOk())
-        .andExpect(view().name("recipe/show"));
->>>>>>> branch 'master' of https://github.com/anis-dh/spring-recipe-app.git
   }
 }
