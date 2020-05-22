6034c06ca3ce21ca41b42c449be7f1f065ce6c0c
anis <anis.dh94@gmail.com>
2020-05-22 23:43:43 +0100

    added dropdown for difficulty / refactoring

--- src/main/java/com/spring/recipe/app/controllers/IngredientController.java
+++ src/main/java/com/spring/recipe/app/controllers/IngredientController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import com.spring.recipe.app.commands.IngredientCommand;
 import com.spring.recipe.app.commands.RecipeCommand;
 import com.spring.recipe.app.commands.UnitOfMeasureCommand;
@@ -30,8 +29,7 @@ public class IngredientController {
     this.unitOfMeasureService = unitOfMeasureService;
   }
 
-  @GetMapping
-  @RequestMapping("/recipe/{recipeId}/ingredients")
+  @GetMapping("/recipe/{recipeId}/ingredients")
   public String listIngredients(@PathVariable String recipeId, Model model) {
     log.debug("Getting ingredient list for recipe id: " + recipeId);
 
@@ -41,8 +39,7 @@ public class IngredientController {
     return "recipe/ingredient/list";
   }
 
-  @GetMapping
-  @RequestMapping("recipe/{recipeId}/ingredient/{id}/show")
+  @GetMapping("recipe/{recipeId}/ingredient/{id}/show")
   public String showRecipeIngredient(@PathVariable String recipeId, @PathVariable String id,
       Model model) {
     model.addAttribute("ingredient",
@@ -50,8 +47,7 @@ public class IngredientController {
     return "recipe/ingredient/show";
   }
 
-  @GetMapping
-  @RequestMapping("recipe/{recipeId}/ingredient/new")
+  @GetMapping("recipe/{recipeId}/ingredient/new")
   public String newRecipe(@PathVariable String recipeId, Model model) {
 
     // make sure we have a good id value
@@ -71,8 +67,7 @@ public class IngredientController {
     return "recipe/ingredient/ingredientform";
   }
 
-  @GetMapping
-  @RequestMapping("recipe/{recipeId}/ingredient/{id}/update")
+  @GetMapping("recipe/{recipeId}/ingredient/{id}/update")
   public String updateRecipeIngredient(@PathVariable String recipeId, @PathVariable String id,
       Model model) {
     model.addAttribute("ingredient",
@@ -93,8 +88,7 @@ public class IngredientController {
         + "/show";
   }
 
-  @GetMapping
-  @RequestMapping("recipe/{recipeId}/ingredient/{id}/delete")
+  @GetMapping("recipe/{recipeId}/ingredient/{id}/delete")
   public String deleteIngredient(@PathVariable String recipeId, @PathVariable String id) {
 
     log.debug("deleting ingredient id:" + id);
